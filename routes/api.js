981cc5e89e7cb966ff193a57b14f5155270460c8
Nick <devnnick@gmail.com@>
2018-05-30 11:18:00 +0300

    finished auth

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,21 @@ mongoose.connect(db, err => {
     }
 })
 
+    function verifyToken(req, res, next){
+        if(!req.headers.authorization){
+            return res.status(401).send('Unauthorized requess')
+        }
+        let token = req.headers.authorization.split(' ')[1]
+    if(token === 'null'){
+        return res.status(401).send('Unauthorized requess')
+        }
+        let payload = jwt.verify(token, 'secretKey')
+        if(!payload){
+            return res.status(401).send('Unauthorized requess')            
+        }
+        req.userId = payload.subject
+    }
+
 router.get('/', (req, res)=>{
     res.send('From API')
 })
